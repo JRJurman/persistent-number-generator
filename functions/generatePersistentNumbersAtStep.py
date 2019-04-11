b8686e7b27d800ae1545b928bffd30d6a55cb272
JRJurman <jrjurman@gmail.com>
2019-04-10 21:54:12 -0400

    don't include original values in final steps

--- functions/generatePersistentNumbersAtStep.py
+++ functions/generatePersistentNumbersAtStep.py
@@ -2,11 +2,23 @@ import numpy as np
 from functions.generatePersistentNumbers import generatePersistentNumbers
 
 def generatePersistentNumbersAtStep(start, step):
-  print('Testing:', start)
+  return generatePersistentNumbersAtStepRec(start, step)
+
+def generatePersistentNumbersAtStepRec(start, step):
   if (step == 0 or len(start) == 0):
     return start
   # recursive call
+  print('Testing:', start)
   nextSteps = generatePersistentNumbers(start)
   uniqueSteps = np.unique(nextSteps)
   uniqueStepsWithoutStart = np.setdiff1d(uniqueSteps, start)
   return generatePersistentNumbersAtStep(uniqueStepsWithoutStart, step - 1)
+
+def generatePersistentNumbersAtStepLoop(start, step):
+  while (step != 0 and len(start) != 0):
+    print('Testing:', start)
+    nextSteps = generatePersistentNumbers(start)
+    uniqueSteps = np.unique(nextSteps)
+    start = np.setdiff1d(uniqueSteps, start)
+    step = step - 1
+  return start
--- functions/generatePersistentNumbersAtStep_test.py
+++ functions/generatePersistentNumbersAtStep_test.py
@@ -7,20 +7,19 @@ class generatePersistentNumbersAtStep_spec(unittest.TestCase):
 
   def test_single_step(self):
     result = generatePersistentNumbersAtStep(np.array([0]), 1)
-    self.assertEqual(result.tolist(), [0, 10, 20, 30, 40, 50, 60, 70, 80, 90])
+    self.assertEqual(result.tolist(), [10, 20, 30, 40, 50, 60, 70, 80, 90])
 
     result = generatePersistentNumbersAtStep(np.array([3]), 1)
-    self.assertEqual(result.tolist(), [3, 13, 31])
+    self.assertEqual(result.tolist(), [13, 31])
 
     result = generatePersistentNumbersAtStep(np.array([9]), 1)
-    self.assertEqual(result.tolist(), [9, 19, 33, 91])
+    self.assertEqual(result.tolist(), [19, 33, 91])
 
   def test_multi_step(self):
     result = generatePersistentNumbersAtStep(np.array([2]), 2)
     self.assertEqual(result.tolist(), [
-        2,  12,  21,  26,  34,  43,  62, 126, 134, 143, 162, 216, 223,
-        232, 261, 314, 322, 341, 413, 431, 612, 621, 37, 73, 137, 173,
-        317, 371, 713, 731
+        26,  34,  37,  43,  62,  73, 126, 134, 137, 143, 162, 173, 216, 223,
+        232, 261, 314, 317, 322, 341, 371, 413, 431, 612, 621, 713, 731.
     ])
 
 if __name__ == '__main__':
