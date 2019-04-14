d24ceda400dfec1793fde4cb550947af5b5278c6
JRJurman <jrjurman@gmail.com>
2019-04-14 17:31:25 -0400

    print with base 10 number and base string

--- functions/mapListToBase.py
+++ functions/mapListToBase.py
@@ -1,7 +1,14 @@
 from functions.toBaseN import toBaseN
 
+def originalAndStringOfBaseN(base):
+  def originalAndString(number):
+    return (number, toBaseN(base)(number))
+  return originalAndString
+
 def mapListToBase(numbers, base = 10):
   """
   convert numpy array into integer of base
+
+  only use for printing
   """
-  return list(map(toBaseN(base), list(map(int, numbers))))
+  return list(map(originalAndStringOfBaseN(base), list(map(int, numbers))))
